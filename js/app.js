447741c2c117c7e150784bb6d60de9f83932691d
dhackelman <drew.hackelman@gmail.com>
2017-01-16 16:52:19 -0500

    Failed attempts at javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log("my js is here")
+var btn =  document.getElementById('btn');
+var ele = document.getElementById('heart');
+
+function favTee(ele, clsName) {
+// split  the class name and index the existing values of the element's classes
+//if you don't find the new clsName inside said index,
+//then you can add it
+	var el = ele.className;
+	el = el.split(' ');
+	if(el.indexOf(clsName) > -1){
+		var cIndex = el.indexOf(clsName);
+		el.splice(cIndex, 1);
+		ele.className = " ";
+	    el.forEach(function(item, index){
+	      ele.className += " " + item;
+	    })
+	}
+	else {
+		el.push(clsName);
+		ele.className = " ";
+	    el.forEach(function(item, index){
+	      ele.className += " " + item;
+	    })
+	}
+}
+
+btn.addEventListener('click', function(){
+	favTee(ele, 'favorited')
+})
